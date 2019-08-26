fdcdfff44a004fa676f061eccf0b82f8b059307d
thomas macfarlaine <tommacfarlaine@gmail.com>
2019-08-26 14:43:48 +0200

    added user_plant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_124138) do
+ActiveRecord::Schema.define(version: 2019_08_26_124257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2019_08_26_124138) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_plants", force: :cascade do |t|
+    t.string "nickname"
+    t.bigint "user_id"
+    t.bigint "plant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_user_plants_on_plant_id"
+    t.index ["user_id"], name: "index_user_plants_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +53,6 @@ ActiveRecord::Schema.define(version: 2019_08_26_124138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_plants", "plants"
+  add_foreign_key "user_plants", "users"
 end
