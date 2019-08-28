6161d3df90c55661e05eb66d6b4b01d36580c1b9
thomas macfarlaine <tommacfarlaine@gmail.com>
2019-08-28 14:44:32 +0200

    search function works

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,10 @@
 class Plant < ApplicationRecord
   has_many :environment_plants
+
+  include PgSearch::Model
+  pg_search_scope :search_all_plants,
+    against: [ :name, :latin_name, :category, :max_height, :care_level, :light_preference, :description, :specifics ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
