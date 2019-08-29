01e95ee35070bd748cd5bea725ec88a3172558fe
thomas macfarlaine <tommacfarlaine@gmail.com>
2019-08-29 18:25:22 +0200

    working with timer fucntion

--- app/jobs/send_reminders_job.rb
+++ app/jobs/send_reminders_job.rb
@@ -2,12 +2,8 @@ class SendRemindersJob < ApplicationJob
   queue_as :default
 
   def perform(reminder_id)
-
     @reminder = Reminder.find(reminder_id)
+
     ReminderMailer.with(user: @reminder.user).reminder.deliver_later
-    if @reminder.frequency == 'once-a-day'
-      new_date = @reminder.date + (20)
-      SendRemindersJob.set(wait_until: new_date).perform_later(reminder.id)
-    end
   end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,14 +4,23 @@ class Reminder < ApplicationRecord
 
   after_create :schedule_reminder
 
+  def self.send_in_the_future(id, date)
+    puts "///////////////////"
+    puts "Rescheduling the job for later..."
+    ReminderMailer.set(wait_until: date).perform_later(id)
+  end
+
   private
 
   def schedule_reminder
     puts "this is the shedule bit"
     unless self.frequency == 'none'
       SendRemindersJob.set(wait_until: self.date).perform_later(self.id)
+
+      if self.frequency == 'once-a-day'
+        new_date = self.date + (20)
+        SendRemindersJob.set(wait_until: new_date).perform_later(self.id)
+      end
     end
   end
-
-
 end
