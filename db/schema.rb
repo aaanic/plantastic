9387b2cac45644969793bbe9d59b5f58ce71fc22
thomas macfarlaine <tommacfarlaine@gmail.com>
2019-08-31 10:44:59 +0200

    working with days but needs time config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_123824) do
+ActiveRecord::Schema.define(version: 2019_08_30_142747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,13 +56,14 @@ ActiveRecord::Schema.define(version: 2019_08_29_123824) do
 
   create_table "reminders", force: :cascade do |t|
     t.string "name"
-    t.datetime "date"
     t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "environment_id"
     t.string "frequency"
+    t.string "weekday"
+    t.integer "hours"
     t.index ["environment_id"], name: "index_reminders_on_environment_id"
     t.index ["user_id"], name: "index_reminders_on_user_id"
   end
