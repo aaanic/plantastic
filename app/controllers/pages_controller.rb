20602fe383cb89f5ae14bc0867eaa86c1737794d
Samira Liebendoerfer <hallo@samiraliebendoerfer.ch>
2019-09-01 11:05:28 +0200

    styling of modal reminders in process, modal works, styling needs to be finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,5 +13,21 @@ class PagesController < ApplicationController
     @reminders = Reminder.all
     @environment = current_user.environment
     @environment_plants = current_user.environment.environment_plants
+
+    # @reminder = Reminder.new
+    # @reminder.user = current_user
+    # @reminder.environment = current_user.environment
+    # authorize @reminder
+    # if @reminder.save
+    #   redirect_to dashboard_path
+    # else
+    #   render :new
+    # end
   end
+
+  # private
+
+  # def reminder_params
+  #   @reminder = params.require(:reminder).permit(:date, :name, :description, :frequency)
+  # end
 end
