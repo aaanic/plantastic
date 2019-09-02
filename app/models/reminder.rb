89a145d0ddfef4f11fd32cb788eb6a3e29f30f36
thomas macfarlaine <tommacfarlaine@gmail.com>
2019-09-02 11:08:44 +0200

    reminders sent at correct time

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -6,21 +6,32 @@ class Reminder < ApplicationRecord
 
   private
 
-  def getDayInTime(weekday)
-    if [1,2,3,4,5,6,7,8,9].include?(self.hours)
-      preWeekday = DateTime.parse(DateTime.parse(weekday).strftime("%Y-%m-%dT0#{self.hours}:00:00%z"))
-    else
-      preWeekday = DateTime.parse(DateTime.parse(weekday).strftime("%Y-%m-%dT#{self.hours}:00:00%z"))
-    end
 
-    (preWeekday < DateTime.parse(DateTime.now.strftime("%Y-%m-%dT00:00:00%z"))) ? (DateTime.parse(weekday) + 7) : DateTime.parse(weekday)
+  def getDayInTime
+    if [1,2,3,4,5,6,7,8,9].include?(self.hours)
+      preWeekday = DateTime.parse(DateTime.parse(self.weekday).strftime("%Y-%m-%dT0#{self.hours}:00:00+02:00"))
+    else
+      preWeekday = DateTime.parse(DateTime.parse(self.weekday).strftime("%Y-%m-%dT#{self.hours}:00:00+02:00"))
+    end
+    if preWeekday < DateTime.parse(DateTime.now.strftime("%Y-%m-%dT00:00:00+02:00"))
+      preWeekday + 7
+    else
+      preWeekday
+    end
   end
 
 
   def schedule_reminder
     unless self.frequency == 'NONE'
-      actualDate = getDayInTime(self.weekday)
-      waitDate = DateTime.parse(actualDate.strftime("%Y-%m-%dT00:00:00%z"))
+      actualDate = getDayInTime
+
+      if [1,2,3,4,5,6,7,8,9].include?(self.hours)
+        waitDate = DateTime.parse(DateTime.parse(self.weekday).strftime("%Y-%m-%dT0#{self.hours}:00:00+02:00"))
+      else
+        waitDate = DateTime.parse(DateTime.parse(self.weekday).strftime("%Y-%m-%dT#{self.hours}:00:00+02:00"))
+      end
+
+      # waitDate = DateTime.parse(actualDate.strftime("%Y-%m-%dT00:00:00%z"))
 
       SendRemindersJob.set(wait_until: waitDate).perform_later(self.id)
 
